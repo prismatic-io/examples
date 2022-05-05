3b3c423344c921147553db9c61db186edff0be40
Taylor Reece <taylor.reece@prismatic.io>
2022-05-05 13:13:55 -0500

    Update data-example to Spectral 6.x. Support JSON inputs and add tests.

--- components/data-example/src/index.test.ts
+++ components/data-example/src/index.test.ts
@@ -1,5 +1,7 @@
-import { myAction } from ".";
-import { invoke } from "@prismatic-io/spectral/dist/testing";
+import component from ".";
+import { ComponentTestHarness } from "@prismatic-io/spectral/dist/testing";
+
+const harness = new ComponentTestHarness(component);
 
 describe("test my action", () => {
   test("verify the return value of my action", async () => {
@@ -8,10 +10,35 @@ describe("test my action", () => {
       price: 1.25,
       quantity: 75,
     };
-    const expectedOutput = "This is an invoice for 75 Widgets at price $1.25. Total price: $93.75";
-    const { result } = await invoke(myAction, {
+    const expectedOutput =
+      "This is an invoice for 75 Widgets at price $1.25. Total price: $93.75";
+    const result = await harness.action("myAction", {
       pointOfSale: sampleInputData,
     });
     expect(result.data).toBe(expectedOutput);
+  });
+
+  test("verify that you can also pass in a JSON string", async () => {
+    const sampleInputJson =
+      '{"productName":"Widget","price":1.25,"quantity":75}';
+    const expectedOutput =
+      "This is an invoice for 75 Widgets at price $1.25. Total price: $93.75";
+    const result = await harness.action("myAction", {
+      pointOfSale: sampleInputJson,
+    });
+    expect(result.data).toBe(expectedOutput);
+  });
+
+  test("verify that an error is thrown when data is not formatted correctly", async () => {
+    const sampleInputData = {
+      productName: "Widget",
+      price: 1.25,
+      amount: 75, // Not "quantity"
+    };
+    expect(async () => {
+      await harness.action("myAction", {
+        pointOfSale: sampleInputData,
+      });
+    }).rejects.toThrow();
   });
 });
--- components/data-example/src/index.ts
+++ components/data-example/src/index.ts
@@ -1,4 +1,10 @@
-import { action, component, input } from "@prismatic-io/spectral";
+import { action, component, input, util } from "@prismatic-io/spectral";
+
+interface PointOfSaleType {
+  productName: string;
+  price: number;
+  quantity: number;
+}
 
 const pointOfSaleInput = input({
   label: "pointOfSaleInput",
@@ -7,29 +13,39 @@ const pointOfSaleInput = input({
   comments:
     "Ensure the input is a deserialized object of the form { productName: string, price: number, quantity: number }.",
   example: "{ productName: 'Widget', price: 1.25, quantity: 75 }",
+  clean: (value: any): PointOfSaleType => {
+    const parsedValue = util.types.isJSON(value) ? JSON.parse(value) : value;
+    if (
+      !(
+        "productName" in parsedValue &&
+        "price" in parsedValue &&
+        "quantity" in parsedValue
+      )
+    ) {
+      throw new Error("The data you input is not in the correct format");
+    }
+    return {
+      productName: util.types.toString(parsedValue?.productName),
+      price: util.types.toNumber(parsedValue?.price),
+      quantity: util.types.toNumber(parsedValue?.quantity),
+    };
+  },
 });
-
-interface PointOfSaleType {
-  productName: string,
-  price: number,
-  quantity: number
-}
 
 export const myAction = action({
   display: {
     label: "My Action",
     description: "Sample action that takes a complex data structure.",
   },
-  perform: async (
-    context,
-    params,
-  ) => {
-    const { productName, quantity, price } = params.pointOfSale as PointOfSaleType;
+  perform: async (context, params) => {
+    const { productName, quantity, price } = params.pointOfSale;
     return {
-      data: `This is an invoice for ${quantity} ${productName}s at price $${price}. Total price: $${price * quantity}`,
+      data: `This is an invoice for ${quantity} ${productName}s at price $${price}. Total price: $${
+        price * quantity
+      }`,
     };
   },
-  inputs: {pointOfSale: pointOfSaleInput},
+  inputs: { pointOfSale: pointOfSaleInput },
 });
 
 export default component({
